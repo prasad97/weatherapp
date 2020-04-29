b1d392bef411ccfd22cb28bd9b2bf9480144bcb8
Prasad Sawant <prasad.sawant20397@gmail.com>
2020-04-29 13:34:34 +0530

    get data and parse into json

--- app.js
+++ app.js
@@ -7,10 +7,22 @@ app.get("/", function(req,res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Kharghar&appid=482b31c6962e567d971dc9cd1b8b7c4c&units=metric";
 
     https.get(url, function(response){
-        console.log(response);
-    });
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+            //console.log(data);
+            const weatherData = JSON.parse(data);
+            //console.log(weatherData);
+            const desc = weatherData.weather[0].description;
+            console.log(desc);
+
+
+        })
+    })
+
+
     res.send("Server is up and running");
-});
+})
 
 
 app.listen(3000, ()=>{
